9d5eb71158c256884232c1365a89b6d8e598a12d
Nadssx <38818513+Nadssx@users.noreply.github.com>
2019-01-31 13:43:35 +0200

    BASIC SYNTAX, CONDITIONAL STATEMENTS AND LOOPS

---
+++ 01CSharpBasicSyntaxConditionalStatementsAndLoops/01P3 Intro-and-Basic-Syntax-More-Exercise/Intro-and-Basic-Syntax-More-Exercise/P02EnglishNameOfTheLastDigit/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace P02EnglishNameOfTheLastDigit
+{
+	class Program
+	{
+		public static void Main()
+		{
+
+			long number = long.Parse(Console.ReadLine());
+
+			number = GetLastDigit(number);
+
+			string[] verbalNums =
+			{
+				"zero",
+				"one",
+				"two",
+				"three",
+				"four",
+				"five",
+				"six",
+				"seven",
+				"eight",
+				"nine"
+			};
+
+			Console.WriteLine(verbalNums[number]);
+
+		}
+
+		private static long GetLastDigit(long number)
+		{
+			long lastDigit = long.Parse(number.ToString().Last().ToString());
+
+			return lastDigit;
+		}
+	}
+}
