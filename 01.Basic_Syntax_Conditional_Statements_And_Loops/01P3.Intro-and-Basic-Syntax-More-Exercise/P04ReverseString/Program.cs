205c1a68f9b7ba0b56fe3e0e8783fdfd91b40286
Nadssx <38818513+Nadssx@users.noreply.github.com>
2019-02-04 17:45:18 +0200

    First commit

---
+++ 01.Basic_Syntax_Conditional_Statements_And_Loops/01P3.Intro-and-Basic-Syntax-More-Exercise/P04ReverseString/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace P04ReverseString
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string input = Console.ReadLine();
+
+			for (int i = input.Length - 1; i >= 0; i--)
+			{
+				Console.Write(input[i]);
+			}
+			Console.WriteLine();
+		}
+	}
+}
