73d82b76cdc101e1d21f7a80a4a5ae88a9344f1b
nadsx <programmernads@gmail.com>
2020-01-04 21:11:29 +0200

    Added CPlusPlus-First-Steps-In-Coding-Exercises

---
+++ C++-Programming_Basics/01.First_Steps_In_Coding/04_RectangleArea/04_RectangleArea.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+    int a;
+    int b;
+
+    a = 2;
+    b = 5;
+
+    int area = a * b;
+
+    cout << area << endl;
+
+    return 0;
+}
